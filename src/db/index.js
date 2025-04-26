435649ee0e15edbe257f81eebc78f4e74a818220
Derek Koloditch <derek.koloditch@gmail.com>
2025-04-26 16:31:46 -0600

    db / docker connection note

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const seedData = require("./seedData.js");
 
 const connectDatabase = async () => {
+  // note the hostame is 'mongodb' to match the docker-compose service name
   const db = await mongoose.connect("mongodb://mongodb:27017/dev");
   await console.log("db connected");
   if (process.env.NODE_ENV === "dev") await seedData();
